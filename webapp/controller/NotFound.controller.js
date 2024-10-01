b38337aced3e02ef6035feca936516454ae1b3ac
Jonathan Nascimento <jonathan_moreira147@hotmail.com>
2024-10-01 16:52:06 +0000

    First commit

---
+++ webapp/controller/NotFound.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "student00/com/sap/training/ux402/listdetail/ux402listdetail/controller/BaseController"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("student00.com.sap.training.ux402.listdetail.ux402listdetail.controller.NotFound", {
+        onInit: function () {
+
+        }
+    });
+});
